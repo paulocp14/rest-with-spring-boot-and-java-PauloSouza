4e8e21970034f373dd50a31fd87275b13bebc220
Paulocp14 <p.souza1411@gmail.com>
2023-09-20 18:09:27 -0300

    EndPoint REST

---
+++ rest-with-spring-boot-and-java-paulosouza/src/main/java/br/com/paulosouza/restwithspringbootandjavapaulosouza/Greeting.java
@@ -0,0 +1,21 @@
+package br.com.paulosouza.restwithspringbootandjavapaulosouza;
+
+public class Greeting {
+
+    private final long Id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        Id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
